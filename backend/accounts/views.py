b401bcead0de17ff5441c3f3c68433a2119d0b26
jpb989 <77682780+jpb989@users.noreply.github.com>
2024-07-15 16:33:10 +0000

    Pending changes exported from your codespace

---
+++ backend/accounts/views.py
@@ -0,0 +1,35 @@
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
+from rest_framework import status
+from django.contrib.auth import authenticate
+from .serializers import LoginSerializer, RegisterSerializer, UserSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+    
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = User.objects.create_user(username=username, password=password)
+        return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
+
+class LoginView(generics.GenericAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = LoginSerializer
+    
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            })
+        return Response({"message": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
